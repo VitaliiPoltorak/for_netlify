cd688eeb16b265963bbff7ef6a57e9ba2c6137cf
ArtMyWeb <vitaliypoltorak91@gmail.com>
2022-08-01 21:48:08 +0300

    tt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+
+        return (
+            <div>
+                <h1>Hello World!</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque aut ea ex minima praesentium saepe
+                    ut. Accusantium deserunt, expedita id labore libero minus, molestiae molestias nemo nobis obcaecati
+                    quam quod.</p>
+            </div>
+        );
 }
-
-export default App;
